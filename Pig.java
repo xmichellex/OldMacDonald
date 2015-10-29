de4d0bafcbe63df23fef0839a2f4a6007fe57655
xmichellex <michellexi123@gmail.com>
2015-10-29 09:54:47 -0700

    im done

--- Pig.java
+++ Pig.java
@@ -2,7 +2,8 @@ class Pig implements Animal
 {     
 	private String myType; 
 	private String mySound; 
-	public Pig(String type, String sound) {
+	public Pig(String type, String sound)
+	 {
 		myType = type; 
 		mySound = sound; 
 	}
